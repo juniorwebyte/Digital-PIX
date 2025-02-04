ae679796c7c0fe4a707a73c41fae70b6dbe7e808
Júnior Alves <39176934+juniorwebyte@users.noreply.github.com>
2025-02-03 22:16:17 -0300

    Delete resources directory

--- resources/views/afiliados/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('afiliados.layout.menuAfiliados')
-
-@section('content')
-    <div class="row">
-        <div class="col-md-12 text-center">
-            <h3>Seja Bem Vindo a sua área de Afiliado!</h3>
-        </div>
-    </div>
-@endsection
